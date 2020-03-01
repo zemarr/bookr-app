2112e5905f892df585a571e7d2ba80b3b6c3c7ea
flint <olusegun.sola19@gmail.com>
2020-03-01 13:30:57 +0100

    checkAvailabilty2

--- client/src/components/dashboard/Mymeetings.js
+++ client/src/components/dashboard/Mymeetings.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import React, {Fragment, useEffect, useState} from 'react';
-import axios from 'axios';
-import { Card } from 'react-bootstrap'
-=======
 import React, { Fragment } from 'react';
 import { TiPencil } from 'react-icons/ti';
 import { AiOutlineClose } from 'react-icons/ai';
->>>>>>> 6a7630a8b041f00b3ad112d1fbdbc94c2e744a78
 
 const Mymeetings = () => {
     const [booked, setBooked] = useState([]);
@@ -31,24 +25,6 @@ const Mymeetings = () => {
     
     return (
         <Fragment>
-<<<<<<< HEAD
-            <div>
-
-                {booked.map((booked, i) => {
-                    return (
-                        <Card style={{ margin: '5px' }}>
-                            <Card.Body>
-                                <div key={i}>
-                                    <p>Meeting Title: {booked.meetingTitle}</p>
-                                    <p>Description: {booked.description} </p>
-                                    <p>Date: {booked.startDate}</p>
-                                   
-                                </div>
-                            </Card.Body>
-                        </Card>
-                    )
-                })}
-=======
             <div className='mymeetings container-fluid'>
                 <p>Scheduled meetings</p>
                 <div className="card w-75 customcard">
@@ -79,7 +55,6 @@ const Mymeetings2 = () => {
                         <p className="card-text">Meeting description</p>
                     </div>
                 </div>
->>>>>>> 6a7630a8b041f00b3ad112d1fbdbc94c2e744a78
             </div>
         </Fragment>
     )
