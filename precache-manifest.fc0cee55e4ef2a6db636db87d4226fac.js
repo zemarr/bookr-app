1f9cb8190da609bc6ba57c46d3d6fc1f0215dddc
flint <olusegun.sola19@gmail.com>
2020-03-03 13:31:36 +0100

    Updates

---
+++ precache-manifest.fc0cee55e4ef2a6db636db87d4226fac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7113cda7fa111cbe5a281d3c2634277",
+    "url": "/bookr/index.html"
+  },
+  {
+    "revision": "32a47de3da3fb894d8ec",
+    "url": "/bookr/static/css/2.d90538a7.chunk.css"
+  },
+  {
+    "revision": "19135a918bd651df6cce",
+    "url": "/bookr/static/css/main.5e14bf0f.chunk.css"
+  },
+  {
+    "revision": "32a47de3da3fb894d8ec",
+    "url": "/bookr/static/js/2.b2b65836.chunk.js"
+  },
+  {
+    "revision": "19135a918bd651df6cce",
+    "url": "/bookr/static/js/main.39577ab2.chunk.js"
+  },
+  {
+    "revision": "db65816bd7d8e8d06395",
+    "url": "/bookr/static/js/runtime-main.d31dbfa2.js"
+  },
+  {
+    "revision": "6e81ea2d39b4a28544430137f72add02",
+    "url": "/bookr/static/media/Boardroom.6e81ea2d.jpeg"
+  },
+  {
+    "revision": "939625fc4dfa8ab0e9f63da010c6bd34",
+    "url": "/bookr/static/media/Hero.939625fc.jpeg"
+  },
+  {
+    "revision": "c6d1a9e56a03ab73111099c946593541",
+    "url": "/bookr/static/media/Marketingroom.c6d1a9e5.jpeg"
+  },
+  {
+    "revision": "50d7a06ddd78077b10852c61779ea497",
+    "url": "/bookr/static/media/Salesroom.50d7a06d.jpeg"
+  },
+  {
+    "revision": "27d8105fea8f4be9482d3a03e8c7917e",
+    "url": "/bookr/static/media/Techroom.27d8105f.jpeg"
+  },
+  {
+    "revision": "c8e40c429a19b298f767ef419a97a079",
+    "url": "/bookr/static/media/aryastark.c8e40c42.jpeg"
+  },
+  {
+    "revision": "af1a238029e9d5bd86e89d2742eb740d",
+    "url": "/bookr/static/media/cerseilannister.af1a2380.jpeg"
+  },
+  {
+    "revision": "4032fe0c43b1dede2e462ab1edc9898a",
+    "url": "/bookr/static/media/jonsnow.4032fe0c.jpeg"
+  }
+]);
